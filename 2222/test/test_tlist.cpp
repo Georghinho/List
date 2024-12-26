d88874701386f67150acef80de62da1ee4c3777b
Georghinho <yurkin23egor@yandex.ru>
2024-12-26 23:31:34 +0300

    List+Queue

--- 2222/test/test_tlist.cpp
+++ 2222/test/test_tlist.cpp
@@ -1,200 +1,366 @@
-#include "tmatrix.h"
-
+#include "tlist.h"
 #include <gtest.h>
 
-TEST(TDynamicMatrix, can_create_matrix_with_positive_length)
-{
-  ASSERT_NO_THROW(TDynamicMatrix<int> m(5));
+//LIST TESTS
+
+TEST(tList, can_create_list_without_parameters) {
+    ASSERT_NO_THROW(List<int> l);
 }
 
-TEST(TDynamicMatrix, cant_create_too_large_matrix)
-{
-  ASSERT_ANY_THROW(TDynamicMatrix<int> m(MAX_MATRIX_SIZE + 1));
+
+TEST(tList, can_create_list_with_positive_length) {
+    ASSERT_NO_THROW(List<int> l(5));
 }
 
-TEST(TDynamicMatrix, throws_when_create_matrix_with_negative_length)
-{
-  ASSERT_ANY_THROW(TDynamicMatrix<int> m(-5));
+
+TEST(tList, cannot_create_list_with_negative_length) {
+    ASSERT_ANY_THROW(List<int> l(-2));
 }
 
-TEST(TDynamicMatrix, can_create_copied_matrix)
-{
-  TDynamicMatrix<int> m(5);
 
-  ASSERT_NO_THROW(TDynamicMatrix<int> m1(m));
+TEST(tList, can_create_list_with_large_length) {
+    ASSERT_NO_THROW(List<long long> l(9999));
 }
 
-TEST(TDynamicMatrix, copied_matrix_is_equal_to_source_one)
-{
-	int size = 7;
-	TDynamicMatrix<int> m(size);
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size - i; j++) {
-			m[i][j] = i + j;
-		}
-	}
-	TDynamicMatrix<int>mat(m);
-	EXPECT_EQ(m, mat);
+
+TEST(tList, test_assignment_operator_equal_length) {
+    List<int> l(2, 0);
+    List<int> l1(2, 0);
+
+    l[1] = 1;
+    l1 = l;
+
+    EXPECT_EQ(l, l1);
 }
 
-TEST(TDynamicMatrix, copied_matrix_has_its_own_memory)
-{
-	const int size = 7;
-	TDynamicMatrix<int> m1(size);
-	for (int i = 0; i < size; i++)
-	{
-		for (int j = 0; j < size - i; j++)
-		{
-			m1[i][j] = i + j;
-		}
-	}
-	TDynamicMatrix<int> m2(m1);
-	EXPECT_NE(&m1, &m2);
-}
-TEST(TDynamicMatrix, can_get_size)
-{
-	TDynamicMatrix<int> m(15);
-	EXPECT_EQ(15, m.size());
 
+TEST(tList, test_assignment_operator_different_length) {
+    List<int> l(2, 0);
+    List<int> l1(5, 0);
+
+    l[1] = 1;
+    l[0] = 23;
+    l1 = l;
+
+    EXPECT_EQ(l, l1);
 }
 
-TEST(TDynamicMatrix, can_set_and_get_element)
-{
-    TDynamicMatrix<int> m(10);
-    for (int i = 0; i < 15; i++)
-    {
-        for (int j = 0; j < 10 - i; j++)
-        {
-            m[i][j] = 1;
-        }
-    }
-    EXPECT_EQ(1, m[0][0]);
-    m[0][0] = 2;
-    EXPECT_EQ(2, m[0][0]);
+
+TEST(tList, test_assignment_operator_equal_lists) {
+    List<int> l(2, 0);
+    List<int> l1(2, 0);
+
+    l[1] = 1;
+    l1[1] = 1;
+    l1 = l;
+
+    EXPECT_EQ(l, l1);
 }
 
-TEST(TDynamicMatrix, throws_when_set_element_with_too_large_index)
-{
-    TDynamicMatrix<int> m(7);
-    ASSERT_ANY_THROW(m.at(0, -1));
-    ASSERT_ANY_THROW(m.at(-1, 0));
-}
-    
 
-TEST(TDynamicMatrix, can_assign_matrix_to_itself)
-{
-    TDynamicMatrix<int> m(10);
-    for (int i = 0; i < 10; i++)
-    {
-        for (int j = 0; j < 4 - i; j++)
-        {
-            m[i][j] = 1;
-        }
-    }
-    ASSERT_NO_THROW(m = m);
+TEST(tList, test_assignment_operator_different_memory) {
+    List<int> l(2, 0);
+    List<int> l1(2, 0);
 
+    l[1] = 1;
+    l1 = l;
+    l1[1] = 10;
+
+    EXPECT_EQ(1, l[1]);
+    EXPECT_EQ(10, l1[1]);
 }
 
-TEST(TDynamicMatrix, can_assign_matrices_of_equal_size)
-{
-    TDynamicMatrix<int> m1(10), m2(10);
-    for (int i = 0; i < 10; i++)
-    {
-        for (int j = 0; j < 10 - i; j++)
-        {
-            m1[i][j] = 1;
-        }
-    }
-    ASSERT_NO_THROW(m2 = m1);
-    EXPECT_EQ(m1, m2);
+
+TEST(tList, test_index_operator) {
+    List<int> l(2, 0);
+    List<int> l1(2, 0);
+
+    l[1] = 1;
+    l1[1] = l[1];
+
+    EXPECT_EQ(l, l1);
 }
 
-TEST(TDynamicMatrix, assign_operator_change_matrix_size)
-{
-    TDynamicMatrix<int> m1(6), m2(4);
-    for (int i = 0; i < 6; i++)
-    {
-        for (int j = 0; j < 6 - i; j++)
-        {
-            m1[i][j] = 1;
-        }
-    }
-    m2 = m1;
-    EXPECT_EQ(m1.size(), m2.size());
+
+TEST(tList, test_index_operator_out_of_range) {
+    List<int> l(2, 0);
+
+    ASSERT_ANY_THROW(l[4]);
 }
 
-TEST(TDynamicMatrix, can_assign_matrices_of_different_size)
-{
-    TDynamicMatrix<int> m1(10), m2(7);
-    for (int i = 0; i < 10; i++)
-    {
-        for (int j = 0; j < 10 - i; j++)
-        {
-            m1[i][j] = 1;
-        }
-    }
-    m2 = m1;
-    ASSERT_NO_THROW(m2 = m1);
-    EXPECT_EQ(m1, m2);
+
+TEST(tList, test_get_size) {
+    List<int> l(2);
+
+    int res = l.size();
+
+    EXPECT_EQ(2, res);
 }
 
-TEST(TDynamicMatrix, compare_equal_matrices_return_true)
-{
-    TDynamicMatrix<int> m1(10), m2(10);
-    for (int i = 0; i < 10; i++)
-    {
-        for (int j = 0; j < 10 - i; j++)
-        {
-            m1[i][j] = 1;
-        }
-    }
-    m2 = m1;
-    EXPECT_EQ(true, m2 == m1);
+
+TEST(tList, test_insert_equal_type) {
+    List<int> l(2);
+    int sz = l.size();
+
+    l.insert(1, 1);
+
+    EXPECT_EQ(1, l[1]);
+    EXPECT_EQ(sz + 1, l.size());
 }
 
-TEST(TDynamicMatrix, compare_matrix_with_itself_return_true)
-{
-    TDynamicMatrix<int> m(7);
-    for (int i = 0; i < 7; i++)
-    {
-        for (int j = 0; j < 7 - i; j++)
-        {
-            m[i][j] = 1;
-        }
-    }
-    EXPECT_EQ(true, m == m);
+
+TEST(tList, test_insert_front) {
+    List<int> l(2);
+    int sz = l.size();
+
+    l.insert_front(1);
+
+    EXPECT_EQ(1, l[0]);
+    EXPECT_EQ(sz + 1, l.size());
 }
 
-TEST(TDynamicMatrix, matrices_with_different_size_are_not_equal)
-{
-    TDynamicMatrix<int> m1(6), m2(10);
-    EXPECT_EQ(false, m2 == m1);
+
+TEST(tList, test_erase) {
+    List<int> l(3, 0);
+    int sz = l.size();
+
+    l[2] = 1;
+    l.erase(1);
+
+    EXPECT_EQ(1, l[1]);
+    EXPECT_EQ(sz - 1, l.size());
 }
 
-TEST(TDynamicMatrix, can_add_matrices_with_equal_size)
-{
-    TDynamicMatrix<int> m1(5);
-    TDynamicMatrix<int> m2(5);
 
-    ASSERT_NO_THROW(m1 + m2);
+TEST(tList, test_erase_front) {
+    List<int> l(3, 0);
+    int sz = l.size();
+
+    l[0] = 1;
+    l.erase_front();
+
+    EXPECT_EQ(0, l[0]);
+    EXPECT_EQ(sz - 1, l.size());
 }
 
-TEST(TDynamicMatrix, cant_add_matrices_with_not_equal_size)
-{
-    TDynamicMatrix<int> m1(7), m2(5);
-    ASSERT_ANY_THROW(m1 + m2); 
+TEST(tList, test_find_true) {
+    List<int> l(3, 0);
+
+    l[2] = 1;
+
+    EXPECT_EQ(2, l.find(1));
 }
 
-TEST(TDynamicMatrix, can_subtract_matrices_with_equal_size)
-{
-    TDynamicMatrix<int> m1(5);
-    TDynamicMatrix<int> m2(5);
 
-    ASSERT_NO_THROW(m1 - m2);
+TEST(tList, test_find_false) {
+    List<int> l(3, 0);
+
+    ASSERT_ANY_THROW(l.find(1));
 }
 
-TEST(TDynamicMatrix, cant_subtract_matrixes_with_not_equal_size)
-{
-    TDynamicMatrix<int> m1(5), m2(7);
-    ASSERT_ANY_THROW(m1 + m2); 
+
+TEST(tList, test_get_first) {
+    List<int> l(3, 0);
+
+    EXPECT_EQ(0, l.get_first());
 }
+
+
+TEST(tList, test_ostream) {
+    List<int> l(2);
+
+    l[1] = 2;
+
+    ASSERT_NO_THROW(std::cout << l);
+}
+
+//ITERATOR TESTS
+
+TEST(tListIterator, can_create_iterator_at_beginning) {
+    List<int> l(3, 0);
+    auto it = l.begin();
+    ASSERT_NO_THROW(*it);
+}
+
+
+TEST(tListIterator, can_create_iterator_at_end) {
+    List<int> l(3, 0);
+    auto it = l.end();
+    ASSERT_NO_THROW(it);
+}
+
+
+TEST(tListIterator, can_dereference_iterator) {
+    List<int> l(3, 0);
+    auto it = l.begin();
+    EXPECT_EQ(*it, 0);
+}
+
+
+TEST(tListIterator, can_increment_iterator_prefix) {
+    List<int> l(3, 0);
+    l[1] = 1;
+    auto it = l.begin();
+    ++it;
+    EXPECT_EQ(*it, 1);
+}
+
+
+TEST(tListIterator, can_increment_iterator_postfix) {
+    List<int> l(3, 0);
+    l[1] = 1;
+    auto it = l.begin();
+    it++;
+    EXPECT_EQ(*it, 1);
+}
+
+TEST(tListIterator, can_compare_iterators_equal) {
+    List<int> l(3, 0);
+    auto it1 = l.begin();
+    auto it2 = l.begin();
+    EXPECT_EQ(it1, it2);
+}
+
+TEST(tListIterator, can_compare_iterators_not_equal) {
+    List<int> l(3, 0);
+    auto it1 = l.begin();
+    auto it2 = l.end();
+    EXPECT_NE(it1, it2);
+}
+
+// Тест на работу оператора ->
+TEST(tListIterator, can_use_arrow_operator) {
+    struct Point {
+        int x;
+        int y;
+    };
+
+    List<Point> l(1, { 1, 2 });
+    auto it = l.begin();
+    EXPECT_EQ(it->x, 1);
+    EXPECT_EQ(it->y, 2);
+}
+
+
+//ZADACHA TESTS
+
+// Тест на нахождение k-ого элемента с конца в списке с несколькими элементами
+TEST(tListFindKthFromEnd, can_find_kth_from_end) {
+    List<int> l;
+    l.append(1);
+    l.append(2);
+    l.append(3);
+    l.append(4);
+    l.append(5);
+
+    EXPECT_EQ(l.find_kth_from_end(0), 5); // Последний элемент
+    EXPECT_EQ(l.find_kth_from_end(1), 4);
+    EXPECT_EQ(l.find_kth_from_end(2), 3);
+    EXPECT_EQ(l.find_kth_from_end(3), 2);
+    EXPECT_EQ(l.find_kth_from_end(4), 1); // Первый элемент
+}
+
+// Тест на нахождение k-ого элемента с конца в списке с одним элементом
+TEST(tListFindKthFromEnd, can_find_kth_from_end_single_element) {
+    List<int> l;
+    l.append(42);
+
+    EXPECT_EQ(l.find_kth_from_end(0), 42);
+}
+
+// Тест на нахождение k-ого элемента с конца в пустом списке
+TEST(tListFindKthFromEnd, cannot_find_kth_from_end_empty_list) {
+    List<int> l;
+
+    ASSERT_ANY_THROW(l.find_kth_from_end(0));
+}
+
+// Тест на нахождение k-ого элемента с конца с недопустимым значением k
+TEST(tListFindKthFromEnd, cannot_find_kth_from_end_out_of_range) {
+    List<int> l;
+    l.append(1);
+    l.append(2);
+    l.append(3);
+
+    ASSERT_ANY_THROW(l.find_kth_from_end(3)); // k больше или равно размеру списка
+    ASSERT_ANY_THROW(l.find_kth_from_end(10)); // k сильно превышает размер списка
+}
+
+//!!!!!!!!!!!!!!!
+//! !Queue tests!
+//! !!!!!!!!!!!!!
+TEST(QueueTest, CanCreateQueueWithDefaultValues) {
+    Queue<int> q;
+    EXPECT_EQ(q.getSize(), 0);
+    EXPECT_TRUE(q.isEmpty());
+}
+
+TEST(QueueTest, CanCreateQueueWithInitialValues) {
+    Queue<int> q(3, 5);
+    EXPECT_EQ(q.getSize(), 3);
+    EXPECT_FALSE(q.isEmpty());
+    EXPECT_EQ(q.front(), 5);
+}
+
+TEST(QueueTest, CanEnqueueElements) {
+    Queue<int> q;
+    q.enqueue(10);
+    EXPECT_EQ(q.getSize(), 1);
+    EXPECT_EQ(q.front(), 10);
+
+    q.enqueue(20);
+    EXPECT_EQ(q.getSize(), 2);
+    EXPECT_EQ(q.front(), 10);  // Front остается прежним
+}
+
+TEST(QueueTest, CanDequeueElements) {
+    Queue<int> q;
+    q.enqueue(10);
+    q.enqueue(20);
+    q.dequeue();
+    EXPECT_EQ(q.getSize(), 1);
+    EXPECT_EQ(q.front(), 20);
+
+    q.dequeue();
+    EXPECT_TRUE(q.isEmpty());
+    EXPECT_EQ(q.getSize(), 0);
+}
+
+TEST(QueueTest, DequeueFromEmptyQueueThrows) {
+    Queue<int> q;
+    EXPECT_THROW(q.dequeue(), std::underflow_error);
+}
+
+TEST(QueueTest, FrontOnEmptyQueueThrows) {
+    Queue<int> q;
+    EXPECT_THROW(q.front(), std::underflow_error);
+}
+
+TEST(QueueTest, CanHandleMultipleOperations) {
+    Queue<int> q;
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+    EXPECT_EQ(q.getSize(), 3);
+    EXPECT_EQ(q.front(), 1);
+
+    q.dequeue();
+    EXPECT_EQ(q.getSize(), 2);
+    EXPECT_EQ(q.front(), 2);
+
+    q.enqueue(4);
+    EXPECT_EQ(q.getSize(), 3);
+    EXPECT_EQ(q.front(), 2);
+
+    q.dequeue();
+    q.dequeue();
+    EXPECT_EQ(q.getSize(), 1);
+    EXPECT_EQ(q.front(), 4);
+
+    q.dequeue();
+    EXPECT_TRUE(q.isEmpty());
+    EXPECT_EQ(q.getSize(), 0);
+}
+
+
+
